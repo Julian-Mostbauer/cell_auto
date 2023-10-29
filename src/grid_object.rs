ad590ca00bd7c23f80ad4a9f326cb02fe9798068
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-30 00:26:44 +0100

    refractored

--- src/grid_object.rs
+++ src/grid_object.rs
@@ -14,21 +14,21 @@ pub struct GridObject {
 
 impl GridObject {
     pub fn new(lenght_param: usize, height_param: usize) -> GridObject {
-        return GridObject {
+        GridObject {
             grid: vec![false; lenght_param * height_param],
             lenght: lenght_param,
             height: height_param,
-        };
+        }
     }
 
     pub fn uniform_to_value(&mut self, value: bool) {
         self.grid = vec![value; self.height * self.lenght];
     }
 
-    pub fn randomize(&mut self){
-        for y in 0..self.height{
-            for x in 0..self.lenght{
-                Self::set_cell(self, rand_range!(0,2) == 1, x, y);
+    pub fn randomize(&mut self) {
+        for y in 0..self.height {
+            for x in 0..self.lenght {
+                Self::set_cell(self, rand_range!(0, 2) == 1, x, y);
             }
         }
     }
@@ -46,53 +46,42 @@ impl GridObject {
         for y in 0..self.height {
             for x in 0..self.lenght {
                 let new_value = Self::rule_set(self, x, y);
-                Self::set_cell(
-                    self,
-                    new_value,
-                    x,
-                    y,
-                )
+                Self::set_cell(self, new_value, x, y)
             }
         }
     }
 
-    fn rule_set(&mut self,x: usize, y: usize) -> bool {
-        let orig_value:bool = self.grid[x + self.lenght * y];
-        let mut surounding_values:Vec<bool> = Vec::new(); 
-        
-        if x > 10 && y > 10{
-            
-        }
+    fn rule_set(&mut self, x: usize, y: usize) -> bool {
+        //let orig_value: bool = self.grid[x + self.lenght * y];
+        let mut surounding_values: Vec<bool> = Vec::new();
 
-        
-        for y_off in -1..=1{
-            for x_off in -1..=1{
-                surounding_values.push(Self::_get_cell(&self, (x as i32 + x_off).max(0) as usize, (y as i32 + y_off).max(0) as usize))
+        for y_off in -1..=1 {
+            for x_off in -1..=1 {
+                surounding_values.push(Self::_get_cell(
+                    self,
+                    (x as i32 + x_off).max(0) as usize,
+                    (y as i32 + y_off).max(0) as usize,
+                ))
             }
         }
 
         let mut living_counter = 0;
 
-        for state in surounding_values{
-            if state{
+        for state in surounding_values {
+            if state {
                 living_counter += 1;
             }
         }
 
-        if living_counter < 2 || living_counter > 4{
-            return true;
-        }else{
-            return false;
-        }
-
+        !(5..=7).contains(&living_counter)
     }
 
-    pub fn get_lenght(&self)->usize{
-        return self.lenght;
+    pub fn get_lenght(&self) -> usize {
+        self.lenght
     }
-    
-    pub fn get_height(&self)->usize{
-        return self.height;
+
+    pub fn get_height(&self) -> usize {
+        self.height
     }
 
     pub fn set_cell(&mut self, value: bool, x: usize, y: usize) {
@@ -100,10 +89,10 @@ impl GridObject {
     }
 
     pub fn _get_cell(&self, x: usize, y: usize) -> bool {
-        if x > self.lenght-1 || y > self.height-1{
+        if x > self.lenght - 1 || y > self.height - 1 {
             return false;
         }
-        return self.grid[x + self.lenght * y];
+        self.grid[x + self.lenght * y]
     }
 
     pub fn _flip_cell(&mut self, x: usize, y: usize) {
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 mod grid_object;
-use std::cell;
 
 use grid_object::GridObject;
 use minifb::{Key, Window, WindowOptions};
@@ -16,9 +15,9 @@ const _YELLOW: u32 = 0xffff00;
 fn main() {
     let fps: f64 = 600.0;
 
-    let mut grid1: GridObject = GridObject::new(450, 450);
+    let mut grid1: GridObject = GridObject::new(100, 100);
     grid1.randomize();
-    
+
     let mut buffer: Vec<u32> = vec![0; WINDOW_WIDTH * WINDOW_HEIGHT];
 
     let mut window = Window::new(
@@ -26,72 +25,81 @@ fn main() {
         WINDOW_WIDTH,
         WINDOW_HEIGHT,
         WindowOptions::default(),
-    ).unwrap_or_else(|e| {panic!("{}", e);});
+    )
+    .unwrap_or_else(|e| {
+        panic!("{}", e);
+    });
 
     // Limit to max ~60 fps update rate
-    window.limit_update_rate(Some(std::time::Duration::from_micros((1.0 / fps * 1_000_000.0) as u64)));
+    window.limit_update_rate(Some(std::time::Duration::from_micros(
+        (1.0 / fps * 1_000_000.0) as u64,
+    )));
 
     let mut loop_count = 0;
     while window.is_open() && !window.is_key_down(Key::Escape) {
         handle_input(&mut window, &mut grid1, loop_count);
         calc_image(&mut buffer, &mut grid1, loop_count);
 
-        window.update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT).unwrap();
+        window
+            .update_with_buffer(&buffer, WINDOW_WIDTH, WINDOW_HEIGHT)
+            .unwrap();
 
         loop_count += 1;
     }
 }
 
 /* INPUT HANDLEING AND PHYSICS -----------------------------------------------------------------------------------------------*/
-fn handle_input(window: &mut Window, grid:&mut GridObject,loop_counter: u32) {
-    if window.is_key_down(Key::R){
+fn handle_input(window: &mut Window, grid: &mut GridObject, _loop_counter: u32) {
+    if window.is_key_down(Key::R) {
         grid.randomize();
         println!("randomized")
     }
-    if window.is_key_down(Key::T){
+    if window.is_key_down(Key::T) {
         grid.uniform_to_value(true);
         println!("set all to true")
     }
-    if window.is_key_down(Key::F){
+    if window.is_key_down(Key::F) {
         grid.uniform_to_value(false);
         println!("set all to false")
     }
 }
 
-fn calc_image(buffer: &mut Vec<u32>, grid:&mut GridObject, loop_counter: u32) {
+fn calc_image(buffer: &mut [u32], grid: &mut GridObject, _loop_counter: u32) {
     clear_buffer(buffer);
-    draw_grid(buffer, grid, loop_counter);
+    draw_grid(buffer, grid, _loop_counter);
     grid.apply_rules();
-
 }
 
-fn draw_grid(buffer: &mut Vec<u32>, grid:&mut GridObject, loop_counter: u32){
-
+fn draw_grid(buffer: &mut [u32], grid: &mut GridObject, _loop_counter: u32) {
     let cell_size = (WINDOW_HEIGHT / grid.get_height()).min(WINDOW_WIDTH / grid.get_lenght());
 
-    for y in 0..grid.get_height(){
-        for x in 0..grid.get_lenght(){
-            match grid._get_cell(x, y){
+    for y in 0..grid.get_height() {
+        for x in 0..grid.get_lenght() {
+            match grid._get_cell(x, y) {
                 true => {
-                    for y_off in 0..cell_size{
-                        for x_off in 0..cell_size{
-                            draw_pixel(buffer, (x*cell_size + x_off) as u32, (y*cell_size + y_off) as u32, _YELLOW);
+                    for y_off in 0..cell_size {
+                        for x_off in 0..cell_size {
+                            draw_pixel(
+                                buffer,
+                                (x * cell_size + x_off) as u32,
+                                (y * cell_size + y_off) as u32,
+                                _YELLOW,
+                            );
                         }
                     }
-                },
-                false => ()
+                }
+                false => (),
             }
-
         }
     }
 }
 
-fn draw_pixel(buffer: &mut Vec<u32>, x: u32, y: u32, color: u32) {
+fn draw_pixel(buffer: &mut [u32], x: u32, y: u32, color: u32) {
     let pixel: usize = (x + ((WINDOW_WIDTH as u32) * y)) as usize;
-    let _ = buffer[pixel] = color;
+    buffer[pixel] = color;
 }
 
-fn clear_buffer(buffer: &mut Vec<u32>) {
+fn clear_buffer(buffer: &mut [u32]) {
     for i in buffer.iter_mut() {
         *i = _BLACK;
     }
